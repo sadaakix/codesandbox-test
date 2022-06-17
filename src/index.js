0a5301793c9094228a40803759c767966730ec92
sadaakix <100124850+sadaakix@users.noreply.github.com>
2022-06-17 23:32:29 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -107,3 +107,42 @@
 // const sayHello = (name = "ゲスト") => console.log(`こんにちは!${name}さん`);
 // sayHello();
 // sayHello("さだ");
+
+/**
+ * スプレッド構文...
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFUnc = (num1, num2) => console.log(num1 + num2);
+
+// sumFUnc(arr1[0], arr1[1]);
+// sumFUnc(...arr1);
+
+// まとめる
+// const arr2 = [1,2,3,4,5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4]; // スプレッド構文による配列のコピー（値渡し）
+// arr6[0] = 100;
+// console.log(arr6); // [100, 20]
+// console.log(arr4); // [10, 20]
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4; // 参照渡し
+// console.log(arr8);
+
+// arr8[0] = 100;
+// console.log(arr8);
+// console.log(arr4);
