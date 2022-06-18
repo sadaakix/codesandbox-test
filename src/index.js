58279481f1ea7ce3f3102a9f3db8849814fc2a4d
sadaakix <100124850+sadaakix@users.noreply.github.com>
2022-06-18 09:45:28 +0900

    論理演算子の意味

--- src/index.js
+++ src/index.js
@@ -199,3 +199,26 @@
 // console.log(checkSum(99, 1));
 // console.log(checkSum(99, 2));
 // console.log(checkSum(100, 0));
+
+/**
+ * 論理演算子の本当の意味
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か２はtrueになります");
+// }
+// if (flag1 && flag2) {
+//   console.log("1も２もはtrueになります");
+// }
+
+// ||は左側がfalsyなら右を返す
+// const num = null;
+// const fee = num || "金額未指定になります";
+// console.log(fee);
+
+// &&は左側がtrueなら右を返す
+// const num2 = 100;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
