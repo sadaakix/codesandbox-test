2d12c2118ed17dd9db56cfd9897cb4012fbea681
sadaakix <100124850+sadaakix@users.noreply.github.com>
2022-06-18 09:38:48 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -176,3 +176,26 @@
 //   }
 // });
 // console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 < 0 ? 'trueです' : 'falseです';
+// console.log(val1);
+
+// const num = 1300;
+
+// console.log(typeof num);
+// const formattedNum =
+//   typeof num === "number"
+//     ? num.toLocaleString()
+//     : `文字列の"${num}"が入力されました。数値を入力してください`;
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
+// };
+// console.log(checkSum(99, 1));
+// console.log(checkSum(99, 2));
+// console.log(checkSum(100, 0));
